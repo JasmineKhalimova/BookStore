31222944c71816d6986ab6c869c36e74cb24973a
Jasminekhalimova <jasmine.khalimova@gmail.com>
2024-06-10 21:50:39 +0100

    finished up with user sign up and added validations

--- back-end/app.js
+++ back-end/app.js
@@ -3,17 +3,21 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { check, validationResult } = require('express-validator');
+
 require('dotenv').config();
 
 const userRoutes = require('./routes/user');
 
 const app = express();
 
+//DB connection
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true // Add this option to avoid deprecation warnings
+    useUnifiedTopology: true
 }).then(() => console.log('DB Connected'));
 
+// middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -1,12 +1,18 @@
 const User = require('../models/user');
+const {errorHandler} = require('../helpers/dbErrorHandler');
 
 exports.signup = async (req, res) => {
-    console.log("req.body", req.body);
+    //console.log("req.body", req.body);
+
     const user = new User(req.body);
+
     try {
         const savedUser = await user.save();
+        user.salt = undefined;
+        user.hashed_password = undefined;
         res.json({ user: savedUser });
     } catch (err) {
-        res.status(400).json({ error: err.message });
+        res.status(400).json({ err: errorHandler(err)   
+        });
     }
 };
