fb302e5ef24180720e471512ca712d8cdc6860a8
Jasminekhalimova <jasmine.khalimova@gmail.com>
2024-09-26 20:03:01 +0100

    worked on menu, created env file for connection and started working on signin page

--- front-end/src/user/Signup.js
+++ front-end/src/user/Signup.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Layout from '../core/Layout';
+import { API } from '../config';
 
-const Signup = () => <div>Signup</div>;
+const Signup = () =>(
+    <Layout title='Sign-up' description='Sign-up page'>
+        {API}
+    </Layout>
+);
 
 export default Signup;
