0bc7d1ae841b0e876b3bf512e54f66bb9a1b3eae
Jasminekhalimova <jasmine.khalimova@gmail.com>
2024-09-30 20:49:38 +0100

    creted category page

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -8,6 +8,7 @@ import Dashboard from './user/UserDashboard';
 import PrivateRoute from './auth/PrivateRoute';
 import AdminRoute from './auth/AdminRoute';
 import AdminDashboard from './user/AdminDashboard';
+import Category from './admin/Category';
  
 const Routes = () => {
     return (
@@ -19,6 +20,7 @@ const Routes = () => {
                 <Route path="/signup" exact component={Signup} />
                 <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
                 <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
+                <AdminRoute path="/create/category" exact component={Category} />
             </Switch>
         </BrowserRouter>
     );
