68b7b2bd2d23dcdbb4c9f6fcd9ce5101804f3df5
Jasminekhalimova <jasmine.khalimova@gmail.com>
2024-10-07 21:35:14 +0100

    single product view

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -8,9 +8,10 @@ import Dashboard from './user/UserDashboard';
 import PrivateRoute from './auth/PrivateRoute';
 import AdminRoute from './auth/AdminRoute';
 import AdminDashboard from './user/AdminDashboard';
-import Category from './admin/Category';
-import Product from './admin/Product';
+import AddCategory from './admin/AddCategory';
+import AddProduct from './admin/AddProduct';
 import Shop from './core/Shop';
+import Product from './core/Product';
  
 const Routes = () => {
     return (
@@ -19,12 +20,13 @@ const Routes = () => {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/shop" exact component={Shop} />
+                <Route path="/product/:productId" exact component={Product} />
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
                 <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
                 <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
-                <AdminRoute path="/create/category" exact component={Category} />
-                <AdminRoute path="/create/product" exact component={Product} />
+                <AdminRoute path="/create/category" exact component={AddCategory} />
+                <AdminRoute path="/create/product" exact component={AddProduct} />
             </Switch>
         </BrowserRouter>
     );
